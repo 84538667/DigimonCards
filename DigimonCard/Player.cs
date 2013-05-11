14636309237019ca24ff38c85585e72877ef7821
HC_Liang <lianghongcheng91@gmail.com>
2013-05-11 17:51:43 +0800

    init

---
+++ DigimonCard/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigimonCard
+{
+    class Player
+    {
+        private int hp;
+        private int cardsNum;
+        private int totalTimes;
+        private int winTimes;
+        private int loseTimes;
+        private String name;
+
+
+    }
+}
---
+++ DigimonCard/TacticsCards.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigimonCard
+{
+    class TacticsCards
+    {
+        private String name;
+        private String stage;
+        private String target;
+
+        public String getNmae()
+        {
+            return name;
+        }
+
+        public String getStage()
+        {
+            return stage;
+        }
+
+        public String getTarget()
+        {
+            return target;
+        }
+
+        public void Fun()
+        {
+        }
+
+    }
+}
